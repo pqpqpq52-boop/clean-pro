0236b7f1852b6befe37e30772742bc040664a0d7
pqpqpq52-boop <pqpqpq52@gmail.com>
2026-04-02 16:57:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,14 @@
-self.addEventListener('install', e=>{
-e.waitUntil(
-caches.open('cp-cache').then(cache=>{
-return cache.addAll([
-'./',
-'./index.html',
-'./manifest.json'
-]);
-})
-);
+const CACHE_NAME = "clean-pro-v1";
+const urlsToCache = ["./","./index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e=>{
-e.respondWith(
-caches.match(e.request).then(res=>{
-return res || fetch(e.request);
-})
-);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
